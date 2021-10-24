413f4f22a4b071fb5ec617e9a86a84205572ce6c
Laura Ricci Calixto <78855094+laricci@users.noreply.github.com>
2021-10-24 11:36:35 -0300

    Adicionar Views

---
+++ resources/views/agenda/remove.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remover Agendamento</title>
+</head>
+<body>
+
+    <h1>Remover Agendamento</h1>
+    
+</body>
+</html>
---
+++ resources/views/area/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Área</title>
+</head>
+<body>
+
+    <h1>Editar Área</h1>
+    
+</body>
+</html>
