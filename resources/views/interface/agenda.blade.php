2ed830472e5295fb8bdb95ef9277ef684ed42000
Laura Ricci <laura@iquattro.com.br>
2021-11-04 14:45:22 -0300

    Atualizar Views Minha Agenda e Login

--- resources/views/interface/agenda.blade.php
+++ resources/views/interface/agenda.blade.php
@@ -4,11 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Minha Agenda</title>
 </head>
 <body>
 
-    <h1>Minha Agenda</h1>
+    <nav>
+        <ul>
+            <li><a class='active' href="">Agenda</a></li>
+            <li><a href="">Perfil</a></li>
+            <li><a href="">Sair</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div class="agenda">
+            <h1>Minha Agenda</h1>
+            <table class='agd'>
+                <tr class="cabecalho">
+                    <th>Data</th>
+                    <th>Status</th>
+                    <th>Treinamento</th>
+                    <th>Acessar</th>
+                </tr>
+                <tr>
+                    <td>12/12/2021</td>
+                    <td>Pendente</td>
+                    <td>Exemplo de treinamento</td>
+                    <td><a href="">Acessar</a></td>
+                </tr>
+                </tr>
+                <tr>
+                    <td>12/12/2021</td>
+                    <td>Pendente</td>
+                    <td>Exemplo de treinamento</td>
+                    <td><a href="">Acessar</a></td>
+                </tr>
+                </tr>
+                <tr>
+                    <td>12/12/2021</td>
+                    <td>Pendente</td>
+                    <td>Exemplo de treinamento</td>
+                    <td><a href="">Acessar</a></td>
+                </tr>
+            </table>
+        </div>
+    </main>
     
 </body>
 </html>
